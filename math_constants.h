94b289acf1752a70ff79f907931adb88d04e400a
Graham Briggs <grahambriggs@gmail.com>
2026-07-23 20:15:52 +0900

    std::numbers::pi & templated deg2rad function

--- math_constants.h
+++ math_constants.h
@@ -1,7 +1,15 @@
 #pragma once
 
-constexpr double PI = 3.14159265;
+#include <numbers>
 
-constexpr float DEG2RAD(float angle) {
-    return angle * (PI / 180.0);
+//constexpr double PI = 3.14159265;
+
+// constexpr float DEG2RAD(float angle) {
+//     //return angle * (PI / 180.0);
+//     return angle * (std::numbers::pi / 180.0);
+// }
+
+template <typename T>
+constexpr T DEG2RAD(T degrees) noexcept {
+    return degrees * (std::numbers::pi_v<T> / static_cast<T>(180));
 }
